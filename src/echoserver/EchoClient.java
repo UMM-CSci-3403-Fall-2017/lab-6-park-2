cf57fed74920b5909944898ba27296a136c5a364
Sungjae Park <sungjaepark@er-10-129-250-46.wireless.umn.edu>
2017-10-24 16:44:49 -0500

    Add code at EchoClient.java file.

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -10,5 +10,41 @@ import java.net.Socket;
 
 public class EchoClient {
 	public static void main(String[] args) {
+		try{
+			String ip = "localhost";
+			int port = 10001;
+			Socket socket = new Socket(ip, port);
+
+			BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+
+			OutputStream out = socket.getOutStream();
+			InputStream in = Socket.getInputStream();
+
+			PrintWriter pw = new PrintWriter(new OutputStreamWriter(out));
+			BufferedReader br = new BufferedReader(new InputStreamReader(in));
+
+			String myMsg = null;
+			String echo = null;
+
+			while((myMsg = input.readLine()) != null) {
+				if(myMsg.equsl("/1")) {
+					break;
+
+				}
+
+				pw.println(myMsg);
+				pw.flush();
+				
+				echo = br.readLine();
+				System.out.println("Server: " + echo;
+
+			}
+
+			pw.close();
+			br.close();
+			socket.close();
+		}catch(Exception e) {
+			e.printStackTrace();
+	}
 
 }
