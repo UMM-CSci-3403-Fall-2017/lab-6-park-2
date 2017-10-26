12d63a228320c1c5f6e570adcbedf6e1cd68cfc0
Sungjae Park <park1637@neoprene.morris.umn.edu>
2017-10-26 11:50:14 -0500

    revised all the EchoServer.java and EchoClient.java still needs work.

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -1,52 +1,12 @@
 package echoclient;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.net.Socket;
+import java.net.*;
+import java.io.*;
 
 public class EchoClient {
 	public static void main(String[] args) {
 		try{
 			String ip = "localhost";
 			int port = 10001;
-			Socket socket = new Socket(ip, port);
 
-			BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 
-			OutputStream out = socket.getOutputStream();
-			InputStream in = socket.getInputStream();
-
-			PrintWriter pw = new PrintWriter(new OutputStreamWriter(out));
-			BufferedReader br = new BufferedReader(new InputStreamReader(in));
-
-			String myMsg = null;
-			String echo = null;
-
-			while((myMsg = input.readLine()) != null) {
-				if(myMsg.equals("/q")) {
-					break;
-
-				}
-
-				pw.println(myMsg);
-				pw.flush();
-				
-				echo = br.readLine();
-				System.out.println("Server: " + echo);
-
-			}
-
-			pw.close();
-			br.close();
-			socket.close();
-		}catch(Exception e) {
-			e.printStackTrace();
-	}
-
-}
-
-}
--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -1,55 +1,43 @@
 package echoserver;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.net.InetAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.net.*;
+import java.io.*;
+
 
 public class EchoServer {
 	public static void main(String[] args) {
 		ServerSocket serverSocket = null;
-		int port = 10001;
-		
+		int port = 6013;
 
-		try{
+		try {
 			serverSocket = new ServerSocket(port);
-			System.out.println(getTime() + "server is ready");
-			
-			Socket socket = serverSocket.accept();
-			InetAddress clientAddress = socket.getInetAddress();
-			System.out.println(getTime() + clientAddress + "client access from");
-			
-			OutputStream out = socket.getOutputStream();
-			InputStream in = socket.getInputStream();
+			System.out.println("server is ready!" +port);
 
-			PrintWriter pw = new PrintWriter(new OutputStreamWriter(out));
-			BufferedReader br = new BufferedReader(new InputStreamReader(in));
+			while (true) {
+				Socket clientSocket = serverSocket.accept();
+				System.out.println("Accepted connection from Client!");
 
-			String recvStr = null;
-			while((recvStr = br.readLine()) != null){
-				System.out.println(getTime() + "received string from client : " + recvStr);
-				pw.println(recvStr);
-				pw.flush();
+				OutputStream out = clientSocket.getOutputStream();
+				InputStream reader = clientSocket.getInputStream();
+
+				int newType;
+
+				while((newType = reader.read()) != -1) {
+					System.out.println(newType);
+
+				}
+
+				out.flush();
+				clientSocket.close();
+
 			}
 
-			pw.close();
-			br.close();
-			socket.close();
-			client.close();
-		}catch(Exception e) {
-			e.printStackTrace();
+		} catch (IOException ioe) {
+			System.out.println("ERROR!");
+			System.out.println(ioe);
+
 		}
 	}
-	static String getTime(){
-		SimpleDateFormat f = new SimpleDateFormat("[hh:mm:ss]");
-		return f.format(new Date());
-	}
+	
 }
 
